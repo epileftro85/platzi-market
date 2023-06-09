bd75c2f857222890983e3c52f5793e8be76053f2
Andres.Clavijo <andres.clavijo@CO-XFQCLXJ4J9.local>
2023-06-08 19:53:59 -0500

    Adding entities

---
+++ src/main/java/com/epileftro/market/persistency/entity/Categoria.java
@@ -0,0 +1,44 @@
+package com.epileftro.market.persistency.entity;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+    @Id
+    @Column(name = "id_categoria")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idCategoria;
+
+    private String descricion;
+    private Boolean estado;
+
+    @OneToMany(mappedBy = "categoria")
+    private List<Producto> productos;
+
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getDescricion() {
+        return descricion;
+    }
+
+    public void setDescricion(String descricion) {
+        this.descricion = descricion;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+}
---
+++ src/main/java/com/epileftro/market/persistency/entity/ComprasProducto.java
@@ -0,0 +1,48 @@
+package com.epileftro.market.persistency.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "compra_productos")
+public class ComprasProducto {
+    @EmbeddedId
+    private ComprasProductoPK id;
+
+    private Integer cantidad;
+
+    private Integer total;
+
+    private Boolean estado;
+
+    @ManyToOne
+    @JoinColumn(name = "id_compra", insertable = false, updatable = false)
+    private Compra compra;
+
+    @ManyToOne
+    @JoinColumn(name = "id_producto", insertable = false, updatable = false)
+    private Producto producto;
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Integer getTotal() {
+        return total;
+    }
+
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+}
